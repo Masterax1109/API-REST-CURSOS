428477d8f276511c36f81cd2bcfeb91354003cdd
masterax1109 <am606axel@gmail.com>
2026-02-13 00:29:32 -0400

    CRUD completo funcional

--- routers/idiomas.js
+++ routers/idiomas.js
@@ -4,6 +4,9 @@ const routerIdiomas = express.Router();
 
 const {Idiomas} = require ('../datos/cursos.js').infoCursos;
 
+//middleware
+routerIdiomas.use(express.json())
+
 //idiomas
 routerIdiomas.get('/', (req, res) => {
     res.send(JSON.stringify(Idiomas));
@@ -33,6 +36,53 @@ routerIdiomas.get('/:lenguaje/:nivel', (req, res) => {
     }
 
     res.send(JSON.stringify(resultados));
+});
+
+routerIdiomas.post('/', (req,res) => {
+    let cursoNuevo = req.body;
+    Idiomas.push(cursoNuevo);
+
+    res.send(JSON.stringify(Idiomas)); 
+});
+
+routerIdiomas.put('/:id', (req,res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = Idiomas.findIndex(curso => curso.id == id);
+
+    if (indice >=0){
+        Idiomas[indice] = cursoActualizado;
+    }
+
+    res.send(JSON.stringify(Idiomas));
+});
+
+routerIdiomas.patch('/:id', (req,res)=> {
+    const infoActualizada = req.body;
+    const id = req.params.id;
+
+    const indice = Idiomas.findIndex(curso => curso.id == id);
+
+    if (indice >=0){
+        const cursoAModificar = Idiomas[indice];
+        Object.assign(cursoAModificar, infoActualizada)
+    }
+
+    res.send(JSON.stringify(Idiomas));
+
+})
+
+routerIdiomas.delete('/:id', (req,res) => {
+
+    const id = req.params.id;
+    const indice = Idiomas.findIndex(curso => curso.id == id);
+
+    if (indice >=0){
+        Idiomas.splice(indice, 1);
+    }
+
+    res.send(JSON.stringify(Idiomas));
 });
 
 module.exports = routerIdiomas;
--- routers/programacion.js
+++ routers/programacion.js
@@ -4,6 +4,9 @@ const routerProgramacion = express.Router();
 
 const {Programacion} = require ('../datos/cursos.js').infoCursos;
 
+//middleware
+routerProgramacion.use(express.json())
+
 //Programacion
 routerProgramacion.get('/', (req, res) => {
     res.send(JSON.stringify(Programacion));
@@ -30,7 +33,54 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
         return res.status(404).send(`No se encontraron cursos con el título ${lenguaje} de nivel ${nivel}`);
     }
 
-    res.send(JSON.stringify(resultados));
+    res.send(JSON.stringify(resultados)); 
+});
+
+routerProgramacion.post('/', (req,res) => {
+    let cursoNuevo = req.body;
+    Programacion.push(cursoNuevo);
+
+    res.send(JSON.stringify(Programacion)); 
+});
+
+routerProgramacion.put('/:id', (req,res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = Programacion.findIndex(curso => curso.id == id);
+
+    if (indice >=0){
+        Programacion[indice] = cursoActualizado;
+    }
+
+    res.send(JSON.stringify(Programacion));
+});
+
+routerProgramacion.patch('/:id', (req,res)=> {
+    const infoActualizada = req.body;
+    const id = req.params.id;
+
+    const indice = Programacion.findIndex(curso => curso.id == id);
+
+    if (indice >=0){
+        const cursoAModificar = Programacion[indice];
+        Object.assign(cursoAModificar, infoActualizada)
+    }
+
+    res.send(JSON.stringify(Programacion));
+
+})
+
+routerProgramacion.delete('/:id', (req,res) => {
+
+    const id = req.params.id;
+    const indice = Programacion.findIndex(curso => curso.id == id);
+
+    if (indice >=0){
+        Programacion.splice(indice, 1);
+    }
+
+    res.send(JSON.stringify(Programacion));
 });
 
 module.exports = routerProgramacion;
